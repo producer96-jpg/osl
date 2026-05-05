315d66194bfcf1e0a9425ce2565c99a6af9662da
producer96 <producer9669@gmail.com>
2026-05-05 23:45:05 +0530

    process.cpp

--- process.cpp
+++ process.cpp
@@ -1,39 +1,21 @@
 #include <iostream>
  #include <unistd.h>
  #include <cstdlib>
-
  using namespace std;
-
  int main()
 {
-
 pid_t pid;
-
-
 // Create child process
-
-fork(); pid
-
+fork(); pid;
 if (pid == 0)
-
 {
-
 cout << "Child Process Running" << endl;
-
 // Terminate child
-
 exit(0);
-
 }
-
 else
-
 {
-
 cout << "Parent Process Running" << endl;
-
 }
-
-
 return 0;
 }
