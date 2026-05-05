dc920e17253b9e2d6d18de26ed7223e583f9330f
producer96 <producer9669@gmail.com>
2026-05-05 23:42:28 +0530

    file managment.cpp

--- file managment.cpp
+++ file managment.cpp
@@ -1,38 +1,23 @@
 #include <iostream>
  #include <fcntl.h>
  #include <unistd.h>
-
  using namespace std;
  int main()
 {
 int fd;
-
 fd = open("demo.txt", O_CREAT | O_WRONLY, 0644);
-
 if (fd == -1)
-
 {
-
 cout << "File creation failed" << endl;
-
 return 1;
-
 }
-
 write(fd, "OS Learning made Easy",20);
-
 cout << "File created successfully" << endl;
-
 close(fd);
-
 int status = unlink("demo.txt");
-
 if (status == 0)
-
 cout << "File deleted successfully" << endl;
-
 else
 cout << "File deletion failed" << endl;
-
 return 0;
 }
