83396f5dfaf7a68b7840bf0d398c83f8b482a369
producer96 <producer9669@gmail.com>
2026-05-07 14:37:32 +0530

    Refactor process.cpp to handle parent and child processes

--- process.cpp
+++ process.cpp
@@ -1,21 +1,27 @@
 #include <iostream>
- #include <unistd.h>
- #include <cstdlib>
- using namespace std;
- int main()
+#include <unistd.h>
+#include <cstdlib>
+
+using namespace std;
+
+int main()
 {
-pid_t pid;
-// Create child process
-fork(); pid;
-if (pid == 0)
-{
-cout << "Child Process Running" << endl;
-// Terminate child
-exit(0);
-}
-else
-{
-cout << "Parent Process Running" << endl;
-}
-return 0;
+    pid_t pid;
+
+    // Create child process
+    pid = fork();
+
+    if (pid == 0)
+    {
+        cout << "Child Process Running" << endl;
+
+        // Terminate child
+        exit(0);
+    }
+    else
+    {
+        cout << "Parent Process Running" << endl;
+    }
+
+    return 0;
 }
